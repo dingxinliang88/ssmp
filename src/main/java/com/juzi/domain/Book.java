30aee1e6adeab26a6cdad756cc6c4eb41547b1ee
juzi <d1741530592@163.com>
2023-02-21 16:23:12 +0800

    add ssmp project

---
+++ src/main/java/com/juzi/domain/Book.java
@@ -0,0 +1,40 @@
+package com.juzi.domain;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import java.io.Serializable;
+import lombok.Data;
+
+/**
+ * 
+ * @TableName tbl_book
+ */
+@TableName(value ="tbl_book")
+@Data
+public class Book implements Serializable {
+    /**
+     * 
+     */
+    @TableId(type = IdType.AUTO)
+    private Integer id;
+
+    /**
+     * 
+     */
+    private String type;
+
+    /**
+     * 
+     */
+    private String name;
+
+    /**
+     * 
+     */
+    private String description;
+
+    @TableField(exist = false)
+    private static final long serialVersionUID = 1L;
+}
---
+++ src/main/java/com/juzi/service/BookService.java
@@ -0,0 +1,16 @@
+package com.juzi.service;
+
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.juzi.domain.Book;
+
+import java.util.List;
+
+public interface BookService {
+    Boolean save(Book book);
+    Boolean update(Book book);
+    Boolean delete(Integer id);
+    Book getById(Integer id);
+    List<Book> getAll();
+    IPage<Book> getPage(int currentPage, int pageSize);
+}
